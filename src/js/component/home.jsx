65ab46d86e91eae7e1fd3d92f59dc33fd3691686
roseshay <165499006+roseshay@users.noreply.github.com>
2024-07-03 14:40:50 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+	const [color, setColor] = useState('red');
 
-//create your first component
-const Home = () => {
+	const handleClick = (selectedColor) => {
+		setColor(selectedColor);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light">
+			<div className="pole"></div>
+			<div className="light-container">
+			 <div
+				className={`light red ${color === 'red' ? 'selected' : ''}`}
+				onClick={() => handleClick('red')}
+			></div>
+			<div
+				className={`light yellow ${color === 'yellow' ? 'selected' : ''}`}
+				onClick={() => handleClick('yellow')}
+			></div>
+			<div
+				className={`light green ${color === 'green' ? 'selected' : ''}`}
+				onClick={() => handleClick('green')}
+			></div>
+			</div>
 		</div>
+
 	);
 };
+ReactDOM.render(<TrafficLight />, document.getElementById('app'));
 
-export default Home;
+export default TrafficLight;
+
+
+
+
